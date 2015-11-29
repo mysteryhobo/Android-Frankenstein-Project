6e1154cb7dec2f184c4e31fd87f79eb0dbd2bc13
MKCY <milan.kornicer@uoit.net>
2015-11-29 16:24:40 -0500

    download PDF

--- app/src/main/java/csci4100/uoit/ca/mobilenoteproject/AttachPopup.java
+++ app/src/main/java/csci4100/uoit/ca/mobilenoteproject/AttachPopup.java
@@ -1,9 +1,18 @@
 package csci4100.uoit.ca.mobilenoteproject;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class AttachPopup extends AppCompatActivity {
 
@@ -34,4 +43,39 @@ public class AttachPopup extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void downloadPdf(View v) {
+        downloadPdf();
+    }
+
+    private void downloadPdf() {
+
+        String extStorageDirectory = Environment.getExternalStorageDirectory()
+                .toString();
+        File folder = new File(extStorageDirectory, "pdf");
+        folder.mkdir();
+        File file = new File(folder, "Read.pdf");
+        try {
+            file.createNewFile();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        Downloader.DownloadFile("https://www.durhamregiontransit.com/Schedules/Documents/0314_Oshawa/0314_401_Simcoe_North_WKDY_S3_M4.pdf", file);
+
+        showPdf();
+    }
+    public void showPdf()
+    {
+        File file = new File(Environment.getExternalStorageDirectory()+"/pdf/Read.pdf");
+        PackageManager packageManager = getPackageManager();
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        testIntent.setType("application/pdf");
+        List list = packageManager.queryIntentActivities(testIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(file);
+        intent.setDataAndType(uri, "application/pdf");
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/csci4100/uoit/ca/mobilenoteproject/Downloader.java
+++ app/src/main/java/csci4100/uoit/ca/mobilenoteproject/Downloader.java
@@ -3,5 +3,42 @@ package csci4100.uoit.ca.mobilenoteproject;
 /**
  * Created by mkcy on 11/29/2015.
  */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class Downloader {
+
+    public static void DownloadFile(String fileURL, File directory) {
+
+        final String file = fileURL;
+        final File dir = directory;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    FileOutputStream f = new FileOutputStream(dir);
+                    URL u = new URL(file);
+                    HttpURLConnection c = (HttpURLConnection) u.openConnection();
+                    c.setRequestMethod("GET");
+                    c.setDoOutput(true);
+                    c.connect();
+
+                    InputStream in = c.getInputStream();
+
+                    byte[] buffer = new byte[1024];
+                    int len1 = 0;
+                    while ((len1 = in.read(buffer)) > 0) {
+                        f.write(buffer, 0, len1);
+                    }
+                    f.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
 }
