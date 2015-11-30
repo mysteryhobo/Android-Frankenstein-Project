3633a21443b5f7e1b30b1b5c42cce823fe66b74b
MKCY <milan.kornicer@uoit.net>
2015-11-30 00:15:11 -0500

    Android manifest changes

--- app/src/main/java/csci4100/uoit/ca/mobilenoteproject/NotePrompt.java
+++ app/src/main/java/csci4100/uoit/ca/mobilenoteproject/NotePrompt.java
@@ -1,7 +1,15 @@
 package csci4100.uoit.ca.mobilenoteproject;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by mkcy on 11/29/2015.
  */
-public class NotePrompt {
+public class NotePrompt extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 }
